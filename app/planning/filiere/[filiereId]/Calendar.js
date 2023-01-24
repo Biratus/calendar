0be92ef6406239c6a4f4f966575bc2c904ccc894
Biratus <clement.birette@hotmail.fr>
2023-01-24 12:35:01 +0100

    [Feature] Export to PDF - Puppeteer

--- app/planning/filiere/[filiereId]/Calendar.js
+++ app/planning/filiere/[filiereId]/Calendar.js
@@ -1,11 +1,10 @@
 "use client";
 
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 import React, {
-  createContext,
-  useCallback,
-  useEffect
+  createContext, useEffect
 } from "react";
 import { useCalendar } from "../../(components)/CalendarProvider";
 import { FiliereView } from "../../(components)/CalendarViews";
@@ -18,10 +17,6 @@ import { useZoom } from "../../../../components/zoom/ZoomProvider";
 import ZoomUI from "../../../../components/zoom/ZoomUI";
 import { mapISO } from "../../../../lib/calendar";
 import { isFormateurMissing } from "../../../../lib/realData";
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-import filiereToPdf from "../../../../components/pdf/planning-filiere/Simple";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const viewWidth = 0.5;
 const minCellHeight = 1.3;
@@ -31,7 +26,7 @@ const FiliereContext = createContext();
 
 export default function CalendarFiliere({ name, data }) {
   const filiereData = mapISO(data, ["start", "end"]);
-  const { showLegend, colorOf } = useLegend();
+  const { showLegend, colorOf} = useLegend();
   const { zoom, loaded } = useZoom();
   const { openMenu } = useCalendar();
 
@@ -39,9 +34,6 @@ export default function CalendarFiliere({ name, data }) {
     () => showLegend([...new Set(filiereData.map(({ theme }) => theme))], true),
     []
   );
-  const exportToPDF = useCallback(() => {
-    pdfMake.createPdf(filiereToPdf({name,modules:filiereData})).download();
-  }, []);
   return (
     <Stack justifyContent="center" alignItems="center" spacing={2}>
       <Typography variant="h2" align="center">
@@ -50,8 +42,7 @@ export default function CalendarFiliere({ name, data }) {
       <Stack direction="row" justifyContent="space-between" sx={{ width: 0.5 }}>
         <GlobalViewLink view={FiliereView.key} />
         <ZoomUI range={5} />
-        <Button onClick={exportToPDF} variant="outlined" size="small">Export to PDF</Button>
-       
+        <Link href={`/api/filiere/${name}/pdf`}>Export to PDF</Link>       
       </Stack>
       <Box sx={{ width: viewWidth + zoom * 0.1 }}>
         {!loaded ? (
